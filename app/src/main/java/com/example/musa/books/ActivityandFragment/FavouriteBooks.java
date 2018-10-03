75a2e388f85f3d2ada890a74ba5f3f018b01e63d
wadva474 <wadva474@gmail.com>
2018-10-03 21:21:31 +0100

    Added Search

---
+++ app/src/main/java/com/example/musa/books/ActivityandFragment/FavouriteBooks.java
@@ -0,0 +1,32 @@
+package com.example.musa.books.ActivityandFragment;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musa.books.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class FavouriteBooks extends Fragment {
+
+
+    public FavouriteBooks() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favourite_books, container, false);
+    }
+
+}
